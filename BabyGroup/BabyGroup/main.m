e2308919e5b832cf80df3750238eebe33b7a074f
BracYang <907039275@qq.com>
2015-12-04 14:47:40 +0800

    第一次上传

---
+++ BabyGroup/BabyGroup/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BabyGroup
+//
+//  Created by 杨帆 on 15/12/4.
+//  Copyright © 2015年 杨帆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
