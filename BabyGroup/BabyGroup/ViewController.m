f55b42bbddcb78c9f487b82f9b01deeb577df125
BracYang <907039275@qq.com>
2015-12-04 15:06:16 +0800

    测试测试

--- BabyGroup/BabyGroup/ViewController.m
+++ BabyGroup/BabyGroup/ViewController.m
@@ -16,9 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-//    97989698609879????
-//    asfasfasfwhatshoulejjiajidewjjw
+//    测试测试
+    
 }
 
 - (void)didReceiveMemoryWarning {
